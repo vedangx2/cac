34a6fa02e6ad8b4679fb10f354c958581814760c
Vedang <vedangcool9@gmail.com>
2026-08-31 19:22:43 -0500

    Bump CACHE_NAME to v3

--- public/sw.js
+++ public/sw.js
@@ -19,7 +19,7 @@
  */
 
 // Bump this string to throw away every old cache after a deploy.
-const CACHE_NAME = 'sideline-screen-v2';
+const CACHE_NAME = 'sideline-screen-v3';
 
 self.addEventListener('install', (event) => {
   // Take over as soon as we're installed rather than waiting for every tab to close.
